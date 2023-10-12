3b8b3d71e5f1a7a39d9e9ac054c48aba2334b712
Abdus Sattar <sasorganicagro@gmail.com>
2023-10-12 18:48:02 +0600

    Update footer component's style

--- src/pages/shared/footer/Footer.jsx
+++ src/pages/shared/footer/Footer.jsx
@@ -2,7 +2,7 @@ import logo from "../../../assets/logo.svg";
 
 const Footer = () => {
   return (
-    <footer className="footer p-10 bg-base-200 text-base-content">
+    <footer className="footer p-10 bg-info-content text-white">
       <aside>
         <img src={logo} alt="logo" />
         <p>
