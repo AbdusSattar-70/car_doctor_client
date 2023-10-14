1e65dfb9dfcf9c2b8671f10044f64b9d36f7b075
Abdus Sattar <sasorganicagro@gmail.com>
2023-10-14 18:43:07 +0600

    Update routes file

--- src/routes/Route.jsx
+++ src/routes/Route.jsx
@@ -1,17 +1,29 @@
 import { createBrowserRouter } from "react-router-dom";
 import Main from "../layout/Main";
 import Home from "../pages/home/home/Home";
+import ErrorPage from "../pages/errorPage/ErrorPage";
+import Login from "../pages/signup_login/Login";
+import SignUp from "../pages/signup_login/SignUp";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Main />,
+    errorElement: <ErrorPage />,
     children: [
       {
         path: "/",
         element: <Home />,
-      }
-    ]
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "sign_up",
+        element: <SignUp />,
+      },
+    ],
   },
 ]);
 
